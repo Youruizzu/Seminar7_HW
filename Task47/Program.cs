134741660366f2e4e0fedaeebcda28bad4cffd81
Sergei Youruizzu <icykle.wolf@gmail.com>
2023-03-08 12:08:13 +0300

    Added Task47 solution

--- Task47/Program.cs
+++ Task47/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Clear();
+
+int[,] CreateMatrix(int rows, int columns, int min, int max){
+    int[,] matrix = new int[rows, columns];
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < columns; j++){
+            matrix[i,j] = new Random().Next(min, max+1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix){
+    for (int rows = 0; rows < matrix.GetLength(0); rows++){
+        for (int columns = 0; columns < matrix.GetLength(1); columns++){
+            Console.Write($"{matrix[rows, columns]} ");
+            }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = CreateMatrix(3, 4, 10, 100);
+PrintMatrix(matrix);
